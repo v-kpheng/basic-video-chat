d6e44dae825ae9090008d6b94eccc9644cd8a683
Kongpheng Nanthavongsa <Kongpheng.Nanthavongsa@vonage.com>
2019-05-16 13:31:07 -0700

    First commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Basic Video Chat
+
+Sets up a basic web client using [ToxBox tutorial](https://tokbox.com/developer/tutorials/web/basic-video-chat/).
+
---
+++ js/app.js
@@ -0,0 +1,52 @@
+// replace these values with those generated in your TokBox Account
+let apiKey;
+let sessionId;
+let token;
+
+// Handling all of our errors here by alerting them
+function handleError(error) {
+  if (error) {
+    alert(error.message);
+  }
+}
+
+// (optional) add server code here
+var SERVER_BASE_URL = 'https://v-kpheng-sample-app.herokuapp.com';
+fetch(SERVER_BASE_URL + '/session').then(function(res) {
+  return res.json()
+}).then(function(res) {
+  apiKey = res.apiKey;
+  sessionId = res.sessionId;
+  token = res.token;
+  initializeSession();
+}).catch(handleError);
+
+function initializeSession() {
+  var session = OT.initSession(apiKey, sessionId);
+
+  // Subscribe to a newly created stream
+  session.on('streamCreated', function(event) {
+    session.subscribe(event.stream, 'subscriber', {
+      insertMode: 'append',
+      width: '100%',
+      height: '100%'
+    }, handleError);
+  });
+
+  // Create a publisher
+  var publisher = OT.initPublisher('publisher', {
+    insertMode: 'append',
+    width: '100%',
+    height: '100%'
+  }, handleError);
+
+  // Connect to the session
+  session.connect(token, function(error) {
+    // If the connection is successful, initialize a publisher and publish to the session
+    if (error) {
+      handleError(error);
+    } else {
+      session.publish(publisher, handleError);
+    }
+  });
+}
